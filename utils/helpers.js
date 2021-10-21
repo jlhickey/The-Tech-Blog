06f167b9eaf412c693a9adf67d1008d93e987f4e
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-21 15:29:26 -0500

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,16 +4,5 @@ module.exports = {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
           date
         ).getFullYear()}`;
-      },
-    format_plural: (word, amount) => {
-      if (amount !== 1) {
-        return `${word}s`;
-      }
-  
-      return word;
     },
-    format_url: url => {
-        return url
-          .replace('http://', '')
-      },
   }
