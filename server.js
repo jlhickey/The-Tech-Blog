c9b526a76b5af9cc4fd500fa14dd19f42f76690a
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-21 15:46:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-//Include the modules required for the express server, sequelize and handlebars
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: 'Super secret secret',
     cookie: { maxAge: 36000 },
