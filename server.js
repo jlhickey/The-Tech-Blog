d256766870eb1a4ca8ad788d330e8b12d5e41e73
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-21 15:40:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
     secret: 'Super secret secret',
     cookie: { maxAge: 36000 },
