2949b43737ba25da6a4af985198611df4e95e5e9
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-21 15:09:18 -0500

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 //Set the routes for the api
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
